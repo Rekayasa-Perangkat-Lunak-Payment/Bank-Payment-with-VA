3aede815a2f05fe89d655bfbeb7f37f88a58f637
Patrik Kurniawan Saputra <surel.patrik@gmail.com>
2024-11-04 22:29:19 +0700

    Add Icon and Pages

---
+++ resources/views/pages/instituteList.blade.php
@@ -0,0 +1,10 @@
+@extends('layout.app')
+
+@section('title', 'Dashboard')
+
+@section('page-title', 'Dashboard Overview')
+
+@section('content')
+    <h1>Welcome to the Dashboard!</h1>
+    <p>Here is your main content for the dashboard page.</p>
+@endsection
