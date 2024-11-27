d309af868b0a78dff24d1e6b22d3bfb58e2deb9c
Patrik Kurniawan Saputra <surel.patrik@gmail.com>
2024-11-27 20:27:19 +0700

    Update InstitutionAdmin.php

--- app/Models/InstitutionAdmin.php
+++ app/Models/InstitutionAdmin.php
@@ -8,6 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class InstitutionAdmin extends Model
 {
     use HasFactory;
+    protected $table = 'institution_admins';
+    protected $fillable = [
+        'user_id',
+        'institution_id',
+        'name',
+        'title',
+    ];
+    
     public function user()
     {
         return $this->belongsTo(User::class);
